e5b6d0fa68596e7f08201861c1171d35a63c0296
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2026-07-15 17:46:54 +0200

    Prepare LibreWPF 0.1.0-preview.17

--- eng/progpu-wpf-windows-anycpu-smoke.ps1
+++ eng/progpu-wpf-windows-anycpu-smoke.ps1
@@ -17,7 +17,7 @@ if ([string]::IsNullOrWhiteSpace($Version)) {
 }
 
 if ([string]::IsNullOrWhiteSpace($Version)) {
-    $Version = "0.1.0-preview.16"
+    $Version = "0.1.0-preview.17"
 }
 
 $PackageDirectory = [System.IO.Path]::GetFullPath($PackageDirectory)

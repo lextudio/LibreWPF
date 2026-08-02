503ff2befd43cb8b2ea0fc6a85cd69078eed09fa
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2026-08-02 20:29:33 +0200

    Prepare LibreWPF 0.1.0-preview.35

--- eng/progpu-preview-package-list.sh
+++ eng/progpu-preview-package-list.sh
@@ -29,10 +29,10 @@ progpu_preview_package_version() {
   local package_id="$1"
   case "${package_id}" in
     LibreWPF.Transport|LibreWPF.ProGPU|LibreWPF.Sdk)
-      printf '%s\n' "${PROGPU_WPF_DEV_PACKAGE_VERSION:-0.1.0-preview.34}"
+      printf '%s\n' "${PROGPU_WPF_DEV_PACKAGE_VERSION:-0.1.0-preview.35}"
       ;;
     *)
-      printf '%s\n' "${PROGPU_WPF_PROGPU_PACKAGE_VERSION:-0.1.0-preview.30}"
+      printf '%s\n' "${PROGPU_WPF_PROGPU_PACKAGE_VERSION:-0.1.0-preview.37}"
       ;;
   esac
 }
